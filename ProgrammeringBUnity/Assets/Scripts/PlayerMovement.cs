68b04a743f6fbc328f13f63ccc22e04f10868e54
KageMunken <tobiasdino.2@gmail.com>
2024-04-08 10:18:40 +0200

    PlayerAnimations

--- ProgrammeringBUnity/Assets/Scripts/PlayerMovement.cs
+++ ProgrammeringBUnity/Assets/Scripts/PlayerMovement.cs
@@ -10,16 +10,27 @@ public class PlayerMovement : MonoBehaviour
 
     float Vertical;
     float Horizontal;
+    float CharacterDirX;
+    float CharacterDirY;
+
     Vector2 movement;
+
+    Animator anim;
+    SpriteRenderer spriteRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-         rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void Update()
     {
+        CharacterDirection();
+        AnimationUpdate();
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
             speed = 4.5f;
@@ -31,7 +42,9 @@ public class PlayerMovement : MonoBehaviour
 
         Horizontal = Input.GetAxisRaw("Horizontal");
         Vertical = Input.GetAxisRaw("Vertical");
-        movement = new Vector2 (Horizontal, Vertical).normalized;
+        movement = new Vector2(Horizontal, Vertical).normalized;
+
+        
     }
 
     // Update is called once per frame
@@ -40,4 +53,56 @@ public class PlayerMovement : MonoBehaviour
         rb.velocity = movement * speed;
     }
 
+    void AnimationUpdate()
+    {
+        anim.SetFloat("X_walk", rb.velocity.x);
+        anim.SetFloat("Y_walk", rb.velocity.y);
+
+        anim.SetFloat("X_idle", CharacterDirX);
+        anim.SetFloat("Y_idle", CharacterDirY);
+
+        if (rb.velocity.magnitude > 0)
+        {
+            anim.SetBool("isWalking", true);
+        }
+
+        else
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+        if (rb.velocity.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (rb.velocity.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+
+    void CharacterDirection()
+    {
+        if (Horizontal > 0)
+        {
+            CharacterDirX = 1;
+            CharacterDirY = 0;
+        }
+        else if (Horizontal < 0)
+        {
+            CharacterDirX = -1;
+            CharacterDirY = 0;
+        }
+
+        if (Vertical > 0)
+        {
+            CharacterDirY = 1;
+            CharacterDirX = 0;
+        }
+        else if (Vertical < 0)
+        {
+            CharacterDirY = -1;
+            CharacterDirX = 0;
+        }
+    }
 }
